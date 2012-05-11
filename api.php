dec3709f92fc9bc590a6dbf2ff2860abd0d11c27
Brenton 'B-Train' Fletcher <i@bloople.net>
2012-05-11 19:35:29 +1000

    Simplify PHP

--- api.php
+++ api.php
@@ -1,11 +1,40 @@
 <?
-include("inc.php");
+ini_set("display_errors", "0");
+
+include("config.php");
+mysql_connect($CONFIG['host'], $CONFIG['username'], $CONFIG['password']);
+mysql_select_db($CONFIG['database']);
+mysql_query("SET NAMES 'utf8'");
+
+function sql($query) {
+  $result = mysql_query($query);
+  
+  if(@mysql_num_rows($result) > 0) {
+    $output = array();
+
+    $row = mysql_fetch_assoc($result);
+    while($row != NULL) {
+      $output[] = $row;
+      $row = mysql_fetch_assoc($result);
+    }
+
+    return (count($output) > 1 ? $output : $output[0]);
+  }
+
+  return NULL;
+}
+
+function esc($str) {
+  return '"'.mysql_real_escape_string($str).'"';
+}
+
+
 
 if($_REQUEST["action"] == "load") {
-  $result = doSql("SELECT * FROM documents WHERE `key` = ".esc($_REQUEST["key"]));
+  $result = sql("SELECT * FROM documents WHERE `key` = ".esc($_REQUEST["key"])." LIMIT 1");
   echo $result["text"];
 }
 else if($_REQUEST["action"] == "save") {
-  $result = doSql("REPLACE INTO documents (`key`, `text`) VALUES(".esc($_REQUEST["key"]).", ".esc($_REQUEST["text"]).")");
+  sql("REPLACE INTO documents (`key`, `text`) VALUES(".esc($_REQUEST["key"]).", ".esc($_REQUEST["text"]).")");
 }
 ?>
